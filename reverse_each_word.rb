598fa50c5dfc42caeca7fd9954aabf32370bd7c1
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 04:05:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,3 @@
 def reverse_each_word(sentence)
-  sentence = sentence.split.collect {|element| element.reverse}
-  sentence.reduce {|element| element.reverse}
+  sentence = sentence.split.collect {|element| element.reverse.to_s}
 end
