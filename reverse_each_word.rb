a87dff0b56ee20729c87adfe7f0afa4a31da931e
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 04:10:47 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,8 @@
+require 'pry'
+
 def reverse_each_word(sentence)
-  sentence.split && sentence.collect {|element| element.reverse}
+  sentence.collect {|element| element.to_s}
+  binding.pry
+  
+  sentence.split.collect {|element| element.reverse}
 end
