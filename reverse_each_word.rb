e9529b3cee812faf6575cbc539841f23385a0540
Michael Rabe <m.trabe96@gmail.com>
2021-01-04 02:21:52 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,4 +1,5 @@
 def reverse_each_word(sentence)
-  sentence.reverse.split
-  sentence.collect {|element| element.reverse}
+  sentence.split (" ")
+  sentence.collect {|element| element.reverse!}
+  sentence.join (" ")
 end
